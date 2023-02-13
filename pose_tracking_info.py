f7790f886afb0d4f658ac450dc4e0a17c00a2d44
junyoung <juny2486@naver.com>
2023-02-13 15:00:33 +0900

    dtw

--- pose_tracking_info.py
+++ pose_tracking_info.py
@@ -31,6 +31,8 @@ def tracking_info(cap, frames, instructor) :
     # 정규화 : https://light-tree.tistory.com/125
     # 각 영상에서 두 랜드마크 사이의 벡터를 단위벡터로 표현.
     def L2normalize(x, y):
+        if (x and y) == 0:
+            return 0, 0
         if ((x or y) == None) :
             return None, None
         result = math.sqrt(x**2 + y**2)
@@ -76,6 +78,25 @@ def tracking_info(cap, frames, instructor) :
             #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             results = pose.process(image)
 
+            try:
+                results.pose_landmarks.landmark
+            except AttributeError:
+                L2_landmarks[l2_idx][0], L2_landmarks[l2_idx][1] = None, None             #11 -> 12    
+                L2_landmarks[l2_idx][2], L2_landmarks[l2_idx][3] = None, None            #14 -> 12
+                L2_landmarks[l2_idx][4], L2_landmarks[l2_idx][5] = None, None            #13 -> 11
+                L2_landmarks[l2_idx][6], L2_landmarks[l2_idx][7] = None, None            #16 -> 14
+                L2_landmarks[l2_idx][8], L2_landmarks[l2_idx][9] = None, None            #15 -> 13
+                L2_landmarks[l2_idx][10], L2_landmarks[l2_idx][11] = None, None          #24 -> 12
+                L2_landmarks[l2_idx][12], L2_landmarks[l2_idx][13] = None, None          #23 -> 11
+                L2_landmarks[l2_idx][14], L2_landmarks[l2_idx][15] = None, None          #23 -> 24
+                L2_landmarks[l2_idx][16], L2_landmarks[l2_idx][17] = None, None          #26 -> 24
+                L2_landmarks[l2_idx][18], L2_landmarks[l2_idx][19] = None, None          #25 -> 23
+                L2_landmarks[l2_idx][20], L2_landmarks[l2_idx][21] = None, None          #28 -> 26
+                L2_landmarks[l2_idx][22], L2_landmarks[l2_idx][23] = None, None          #27 -> 25
+                
+                l2_idx += 1
+
+                continue
             # 모든 랜드마크를 벡터화합니다.
             for idx, land in enumerate(results.pose_landmarks.landmark):
                 if idx in [0,1,2,3,4,5,6,7,8,9,10,17,18,19,20,21,22,29,30,31,32]:
